6c35096c657f31e5ce6be0ef3837d83c7241e6cd
Everton Carniato Firmino de Oliveira <everton.carniato@gmail.com>
2020-03-21 15:11:05 -0300

    Criando duas telas de formulario

---
+++ global.js
@@ -0,0 +1,9 @@
+function next() {
+    document.getElementById("professional").style.display = "none"
+    document.getElementById("place").style.display = "block"
+}
+
+function back() {
+    document.getElementById("place").style.display = "none"
+    document.getElementById("professional").style.display = "block"    
+}
